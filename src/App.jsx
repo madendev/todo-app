f5fe3a8689a81b89a4b0607451f3f824c9fd7c38
Manuel Denis <manueldenis.developer@gmail.com>
2025-07-25 14:28:20 +0200

    chore: setup project with Vite, React, SCSS and JSON Server

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [tareas, setTareas] = useState([]);
+
+  return (
+    <>
+      <p>Hola Mundo</p>
+    </>
+  );
+}
+
+export default App;
